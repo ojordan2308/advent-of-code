aedca58aff94ac09ecc5b8ccd5dbead912a37c5e
Oliver Jordan <o.jordan2308@gmail.com>
2023-06-06 17:49:04 +0100

    20 day 2 part 1

---
+++ AoC_20/days/2.py
@@ -0,0 +1,46 @@
+import re
+
+
+PUZZLE_INPUT_PATH = "./inputs/2.txt"
+TEST_INPUT_PATH = "./inputs/2-test.txt"
+
+
+def get_puzzle_input(input_path: str = PUZZLE_INPUT_PATH) -> list[dict]:
+    parsed_input = []
+    with open(input_path) as input:
+        for line in input:
+            line = line.rstrip('\n')
+
+            password = {}
+            password["range"] = tuple(map(lambda x: int(x),
+                                          re.search("\d+-\d+", line).group(0).split("-")))
+            password["letter"] = re.search(".(?=:)", line).group(0)
+            password["password"] = re.search("(?<=: ).*", line).group(0)
+
+            parsed_input.append(password)
+    return parsed_input
+
+
+def find_valid_passwords(passwords: list[dict]) -> int:
+    count = 0
+    for p in passwords:
+        password = p["password"]
+        min, max = p["range"]
+        letter = p["letter"]
+
+        n = password.count(letter)
+        if n >= min and n <= max:
+            count += 1
+    return count
+
+
+
+
+
+if __name__ == "__main__":
+    
+    input = get_puzzle_input()
+    part1_result = find_valid_passwords(input)
+
+    print("======= PART 1 =======")
+    print(part1_result)
